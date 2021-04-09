6f177f840b466a8fecd4afc3874e97d94bbb0063
Ahmad-Abdou <kyoub1996@gmail.com>
2021-04-09 15:07:37 +0200

    Exercise 1,2,3 in Spring MVC Exercises and Thymeleaf

--- src/main/java/com/example/springmvc/demo/controllers/Home.java
+++ src/main/java/com/example/springmvc/demo/controllers/Home.java
@@ -1,43 +1,67 @@
 package com.example.springmvc.demo.controllers;
 
+import com.example.springmvc.demo.dto.ContactDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class Home {
-    List<String> StringList = new ArrayList<>();
+    List<ContactDto> contactDtoList;
+    List<String> aboutList;
     @PostConstruct
     public void text(){
-        StringList = new ArrayList<>();
+        contactDtoList=new ArrayList<>();
+        aboutList = new ArrayList<>();
     }
 
     @GetMapping("/index")
     public String index(){
         return "index";
     }
-
+    @GetMapping("/aboutIndex")
+    public String aboutIndex(){
+        return "aboutIndex";
+    }
 
     @GetMapping("/contact")
-    public String contact(){
+    public String contact(Model model){
+        ContactDto dtoField = new ContactDto();
+        model.addAttribute("dtoField",dtoField);
         return "contact";
     }
-    @PostMapping("/contact/post")
-    public String contactPost(@RequestParam ("test" )String test){
-        System.out.println("test = " + test);
-        StringList.add(test);
-        return "redirect:/index";
+    @GetMapping("/about")
+    public String about(Model model){
+        return "about";
     }
     @GetMapping("/")
     public String contactList(Model model){
-      List<String> StringList = new ArrayList<>();
-        model.addAttribute("StringList",StringList);
+        model.addAttribute("contactDtoList",contactDtoList);
         return "index";
     }
+
+    @GetMapping("/aboutIndex/addingList")
+    public String aboutGet(Model model){
+        model.addAttribute("aboutList",aboutList);
+        return "aboutIndex";
+    }
+
+    @PostMapping("/about/post")
+    public String aboutPost(String input , String email){
+        aboutList.add(input);
+        aboutList.add(email);
+        System.out.println(input);
+        return "redirect:/aboutIndex/addingList";
+    }
+    @PostMapping("/contact")
+    public String contactPost( ContactDto dto){
+        contactDtoList.add(dto);
+        return "redirect:/";
+    }
+
 }
